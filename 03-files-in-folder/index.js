6464944c0cdc4b8bd5016f34bb6ffc670d66f652
rakr0819 <raman.krauchanka@netcracker.com>
2023-05-07 13:08:05 +0300

    fead: add check files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+fs.promises.readdir(secretFolderPath, {withFileTypes: true})
+  .then(data => {
+    data.forEach(item => {
+      const filePath = path.join(secretFolderPath, item.name);
+      const fileNameData = item.name.split('.');
+
+      item.isFile() && fs.stat(filePath, (err, stats) =>
+        console.log(`${fileNameData[0]} - ${fileNameData[1]} - ${stats.size} bytes`)
+      )
+    });
+  })
